f28bd1bd23dbc9d03085c9e3e43f9119866d0b65
edwin <edwinshi2028@u.northwestern.edu>
2025-07-01 14:41:27 -0500

    stretch completed

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,9 @@
 print("Welcome to AI Applications!")
 name = input("Please enter your name: ")
 print("Hello " + name + "! It's great to meet you!")
+
+print()
+
+print("What is your favorite programming language?")
+language = input("Please enter your favorite programming language: ")
+print("That's cool, I love coding in " + language + " too!")
